ee0b068d71899b9c02fd37ba845399077a61b74d
Loqui67 <loicwalter88@gmail.com>
2021-11-26 16:50:28 +0100

    debut du mappage client

---
+++ Projet-POO/Client_mapTB.h
@@ -0,0 +1,32 @@
+#pragma once
+using namespace System;
+
+
+	ref class Client_mapTB
+	{
+	private:
+		//String^ sSql;
+		int Id;
+		String^ nom;
+		String^ prenom;
+	public:
+		String^ Select(void);
+		String^ Insert(void);
+		String^ Delete(void);
+		String^ Update(void);
+		void setId(int);
+		void setNom(String^);
+		void setPrenom(String^);
+		void setDateNaissance(String^);
+
+
+
+
+		int getId(void);
+		String^ getNom(void);
+		String^ getPrenom(void);
+		String^ getDateNaissance(void);
+
+	};
+
+
