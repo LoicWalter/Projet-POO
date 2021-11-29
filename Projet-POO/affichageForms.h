c1d12ccbfc268433a329f9c9ade478491bd8b3b9
Elindrae <sarah.metzger@viacesi.fr>
2021-11-29 17:58:54 +0100

    To do dans afficherform

--- Projet-POO/affichageForms.h
+++ Projet-POO/affichageForms.h
@@ -12,3 +12,23 @@ public:
 
 };
 
+/* A faire encore :
+classe:
+- adresse_map/service
+- article *
+- stocks *
+- commande *
+- payement *
+- personnel *
+
+-------- Autre ---------
+- faire lien entre adresse et client/personnel
+\->changer diagramme classe et voir avec tuteur si bon chemin
+- finir interface pour les 4 autres gestions
+- rajouter les noms boutons manquants interface 1
+- ajouter les click bouton pour client
+- ajouter les dates premieres commande a client_map et service
+
+plutard:
+- click bouton pour les autres 
+- fermer fenetre et retour au menu*/
