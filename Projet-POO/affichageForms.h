44b4721b75725b650bf06f6a2f3557ffc9823b6f
Elindrae <sarah.metzger@viacesi.fr>
2021-12-01 23:19:41 +0100

    interface commande

--- Projet-POO/affichageForms.h
+++ Projet-POO/affichageForms.h
@@ -21,28 +21,28 @@ classe:
 - payement *			fait
 - personnel *			fait
 
--------- Autre ---------
+-------- Autre ---------											------- Etat des taches -------
 - faire lien entre adresse et client/personnel						fait mais a revoir les statics
 \->changer diagramme classe et voir avec tuteur si bon chemin		fait
 - finir interface pour les 4 autres gestions						fait
 - rajouter les noms boutons manquants interface 1					fait
 - ajouter les click bouton pour client								fait
 - ajouter les dates premieres commande a client_map et service		fait mais a revoir
-- refaire interface commande pour rajouter facture et payement		
+- refaire interface commande pour rajouter facture et payement		...
 - enlever adresse dans interface client								fait
 - rajouter commentaire												en cours
-- mettre en place la facture
+- mettre en place la facture										...
 - click bouton pour les autres										en cours
-- fermer fenetre et retour au menu
+- fermer fenetre et retour au menu									...
 - revoir BDD														fait (avec sylvain)
 - requetes clients													fait
 - requetes personnels												fait a revoir pour update
 - requetes pour les statistiques									en cours
-- requetes commande
+- requetes commande													...
 - requetes articles													fait
-- faire les variations 
-- test unitaire / fonctionnel
+- faire les variations												...
+- test unitaire / fonctionnel										...
 /-> simple unitaire utiliser vs
 
-REFERENCE -> voir comment recuperer les premieres lettres, date etc...
+REFERENCE -> voir comment recuperer les premieres lettres,ETC...    ...
 */						
